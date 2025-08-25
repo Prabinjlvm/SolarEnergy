c5e1b4016a5c06de61bf97fb6b0c75257bd5707e
Prabinjlvm <josephprabinjose@gmail.com>
2025-08-25 21:44:35 +0900

    ScrollToTop component added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Project from './pages/Project';
 import ProjectDetails from './pages/ProjectDetails';
+import ScrollToTop from './component/ScrollToTop';
 
 function App() {
   return (
-    <Routes>
+
+    <>
+     <ScrollToTop />
+     <Routes>
      <Route path="/" element={<Home />} />
      <Route path="/project" element={<Project />} />
      <Route path="/project-details" element={<ProjectDetails />} />
     </Routes>
+    </>
+   
   );
 }
 
